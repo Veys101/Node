8f799378c5061b6a2c8e0009ffd5237554c74187
Veys101 <vys2626@gmail.com>
2022-07-23 22:52:55 +0300

    chunk module added

--- Note-App/app.js
+++ Note-App/app.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const utils = require('./utils.js')
 const validator = require('validator')
+const chalk = require('chalk')
 
 // fs.writeFileSync("notes.txt", "This file was created by Node.js")
 // fs.appendFileSync("notes.txt", "\nHello World...")
@@ -10,3 +11,5 @@ console.log(utils.name)
 console.log(utils.add(5, 3))
 
 console.log(validator.isEmail('veys@gmail.com'))
+
+console.log(chalk.blue("Hello World"))
