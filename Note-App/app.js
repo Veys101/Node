7d5e9e23e9865b38fbfe83bcadba2cca943f30f7
Veys101 <vys2626@gmail.com>
2022-07-23 20:20:30 +0300

    utils added

--- Note-App/app.js
+++ Note-App/app.js
@@ -1,4 +1,7 @@
 const fs = require('fs')
+const utils = require('./utils.js')
 
 // fs.writeFileSync("notes.txt", "This file was created by Node.js")
 fs.appendFileSync("notes.txt", "\nHello World...")
+
+console.log(utils.name)
