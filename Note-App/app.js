3cdb78086afaae0a6f10ed231baeabf623dce165
Veys101 <vys2626@gmail.com>
2022-07-23 22:34:56 +0300

    validator module added

--- Note-App/app.js
+++ Note-App/app.js
@@ -1,9 +1,12 @@
 const fs = require('fs')
 const utils = require('./utils.js')
+const validator = require('validator')
 
 // fs.writeFileSync("notes.txt", "This file was created by Node.js")
-fs.appendFileSync("notes.txt", "\nHello World...")
+// fs.appendFileSync("notes.txt", "\nHello World...")
 
 console.log(utils.name)
 
 console.log(utils.add(5, 3))
+
+console.log(validator.isEmail('veys@gmail.com'))
