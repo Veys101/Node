0e55a189fdf086516520dd2fee67a3246de4ae55
Veys101 <vys2626@gmail.com>
2022-07-25 15:43:00 +0300

    yargs added

--- Note-App/app.js
+++ Note-App/app.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const utils = require('./utils.js')
 const validator = require('validator')
 const chalk = require('chalk')
+const yargs = require('yargs')
 
 // fs.writeFileSync("notes.txt", "This file was created by Node.js")
 // fs.appendFileSync("notes.txt", "\nHello World...")
@@ -14,6 +15,34 @@ const chalk = require('chalk')
 
 // console.log(chalk.bold.inverse.blue("Hello World"))
 
-console.log(process.argv)
+// console.log(process.argv)
 
-console.log(process.argv[2] === "add" ? "Adding note!" : "Not Adding note!")
+// console.log(process.argv[2] === "add" ? "Adding note!" : "Not Adding note!")
+
+// console.log(yargs.argv)
+
+/* Customize yargs version */
+yargs.version('1.1.1')
+
+/* Create add command */
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Adding a new note!!')
+    }
+
+})
+
+/* Create remove command */
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a new note',
+    handler: function () {
+        console.log('Removing a new note!!')
+    }
+
+})
+
+
+console.log(yargs.argv)
