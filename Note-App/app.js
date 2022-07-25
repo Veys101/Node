0521a6bf0f9ad0ef031ec3106095024a9be8a6c2
Veys101 <vys2626@gmail.com>
2022-07-25 11:51:45 +0300

    get data with argv

--- Note-App/app.js
+++ Note-App/app.js
@@ -6,10 +6,14 @@ const chalk = require('chalk')
 // fs.writeFileSync("notes.txt", "This file was created by Node.js")
 // fs.appendFileSync("notes.txt", "\nHello World...")
 
-console.log(utils.name)
+// console.log(utils.name)
 
-console.log(utils.add(5, 3))
+// console.log(utils.add(5, 3))
 
-console.log(validator.isEmail('veys@gmail.com'))
+// console.log(validator.isEmail('veys@gmail.com'))
 
-console.log(chalk.bold.inverse.blue("Hello World"))
+// console.log(chalk.bold.inverse.blue("Hello World"))
+
+console.log(process.argv)
+
+console.log(process.argv[2] === "add" ? "Adding note!" : "Not Adding note!")
