62718ab540f272d2af04448f27042715a2fa00c0
Veys101 <vys2626@gmail.com>
2022-07-26 16:15:10 +0300

    arrow function added

---
+++ Note-App/playground/3-arrow-challenge.js
@@ -0,0 +1,24 @@
+const tasks = {
+    tasks: [
+        {
+            text: 'Grocery Shopping',
+            completed: true
+        },
+        {
+            text: 'Clean yard',
+            completed: false
+        },
+        {
+            text: 'Film course',
+            completed: false
+        } 
+    ],
+    getTasksToDo() {
+        const tasksToDo = this.tasks.filter((task) => {
+            return task.completed === false
+        })
+        return tasksToDo
+    }
+}
+
+console.log(tasks.getTasksToDo())
