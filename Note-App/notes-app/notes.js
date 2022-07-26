42b29f18589c68c6a1b2151b5559e3faf99fc30d
Veys101 <vys2626@gmail.com>
2022-07-26 17:40:21 +0300

    debugger added

--- Note-App/notes-app/notes.js
+++ Note-App/notes-app/notes.js
@@ -14,6 +14,8 @@ const addNote = function (title, body) {
     const duplicateNotes = notes.filter(function(note) {
         return note.title === title
     })
+
+    debugger
     
     if(duplicateNotes.length === 0){
         notes.push({
