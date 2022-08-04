46f6b02f96ea135f8158fe3ea29f71b254ec30ff
Veys101 <vys2626@gmail.com>
2022-08-04 16:58:11 +0300

    argv taken from command line

--- Weather-App/forecast-app.js
+++ Weather-App/forecast-app.js
@@ -1,7 +1,9 @@
 const forecast = require("./utils/forecast.js")
 const geocode = require("./utils/geocode.js")
 
-geocode("Eskisehir", (error, response) => {
+const address = process.argv[2]
+
+geocode(address, (error, response) => {
     console.log(error)
     console.log(response)
     forecast(response.latitude, response.longtitude, (error, response) => {
@@ -10,7 +12,7 @@ geocode("Eskisehir", (error, response) => {
     })
 })
 
-forecast(39.766193, 30.526714, (error, response) => {
-    console.log(error)
-    console.log(response)
-})
+// forecast(39.766193, 30.526714, (error, response) => {
+//     console.log(error)
+//     console.log(response)
+// })
