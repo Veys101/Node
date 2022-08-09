e77f8b371c8f55e33351b6c960a7f5a7e31e7f00
Veys101 <vys2626@gmail.com>
2022-08-10 00:57:38 +0300

    html css js added

--- Weather-App/web-server/src/app.js
+++ Weather-App/web-server/src/app.js
@@ -1,6 +1,11 @@
+const path = require('path')
 const express = require("express")
 
+const publicDirectoryPath = path.join(__dirname, '../public')
+
 const app = express()
+
+app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.send('Hello Express!')
