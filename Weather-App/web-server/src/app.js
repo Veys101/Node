d7d43483f41535ca584a2dcd7864c5b71e06b530
Veys101 <vys2626@gmail.com>
2022-08-12 01:17:44 +0300

    weather endpoint added

--- Weather-App/web-server/src/app.js
+++ Weather-App/web-server/src/app.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const express = require("express")
 const hbs = require('hbs')
+const request = require('request')
 
 const app = express()
 
@@ -8,7 +9,7 @@ const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialPath = path.join(__dirname, '../templates/partial')
-
+const url = "http://api.weatherstack.com/current?access_key=acc670a05350333481c211a21cdb16f1&query="
 
 // Setup handlebars engine and views location
 app.set('views', viewsPath)
@@ -40,6 +41,30 @@ app.get('/about', (req, res) => {
         title: 'About Me',
         name: 'Veys'
     })
+})
+
+app.get('/weather', (req, res) => {
+
+    if(!req.query.search) {
+        res.send({
+            error: 'You must provide a search term'
+        })
+    } else {
+        request({url: url+req.query.search, json: true}, (error, response) => {
+            res.send( {
+                product: {
+                    latitude: response.body.location.lat,
+                    longtitude: response.body.location.lon,
+                    regionName: response.body.location.region + "/" + response.body.location.country,
+                    temperature: response.body.current.temperature,
+                    weatherDescription: response.body.current.weather_descriptions,
+                    humidity: response.body.current.humidity,
+                    windSpeed: response.body.current.wind_speed
+                }
+            })
+        })
+    }
+    
 })
 
 app.get('*', (req, res) => {
